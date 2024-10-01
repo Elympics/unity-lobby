a033f290a8abb514ac8ad9b7ee62b179d5fb7722
Patryk Chojnicki <patryk.chojnicki@daftmobile.com>
2024-10-01 10:49:00 +0200

    feat: Replace sample async gameplay from ctz to minimal gameplay

---
+++ Samples~/AsyncGameSample/Gameplay/Scripts/EndGameView.cs
@@ -0,0 +1,46 @@
+﻿using Elympics;
+using ElympicsLobbyPackage.Sample.AsyncGame;
+using TMPro;
+using UnityEngine;
+using Elympics.Models.Authentication;
+using Cysharp.Threading.Tasks;
+using UnityEngine.SceneManagement;
+using JetBrains.Annotations;
+
+public class EndGameView : MonoBehaviour
+{
+    private readonly string formattedRespectMessage = "You got {0} respect";
+
+    [SerializeField] private int lobbySceneIndex = 0;
+    [SerializeField] private TextMeshProUGUI respectText;
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+        DisplayRespect().Forget();
+    }
+
+    [UsedImplicitly] // by BackToLobbyButton
+    public void ReturnToLobby()
+    {
+        SceneManager.LoadScene(lobbySceneIndex);
+    }
+
+    private async UniTask DisplayRespect()
+    {
+        if (ElympicsLobbyClient.Instance == null
+               || !ElympicsLobbyClient.Instance.IsAuthenticated
+               || ElympicsLobbyClient.Instance.AuthData.AuthType is AuthType.None or AuthType.ClientSecret)
+        {
+            respectText.text = "Log in to earn respect";
+        }
+        else
+        {
+            var respectService = new RespectService(ElympicsLobbyClient.Instance, ElympicsConfig.Load());
+            var matchId = FindObjectOfType<PersistentLobbyManager>().CachedMatchId;
+            var respectValue = await respectService.GetRespectForMatch(matchId);
+
+            respectText.text = string.Format(formattedRespectMessage, respectValue.Respect.ToString());
+        }
+    }
+}
