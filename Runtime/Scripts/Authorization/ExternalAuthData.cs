414eff41a2c411c01cefe125fed7ff2639c48490
Krzysztof Pięta <krzysztof.pieta@elympics.cc>
2024-10-01 17:13:20 +0200

    feat: add tournament data in handshake

--- Runtime/Scripts/Authorization/ExternalAuthData.cs
+++ Runtime/Scripts/Authorization/ExternalAuthData.cs
@@ -1,22 +1,31 @@
 #nullable enable
+using Elympics;
 using Elympics.Models.Authentication;
 namespace ElympicsLobbyPackage.Authorization
 {
-	public struct ExternalAuthData
-	{
-		public readonly AuthData? AuthData;
-		public readonly bool IsMobile;
-		public readonly Capabilities Capabilities;
-		public readonly string Environment;
+    public struct ExternalAuthData
+    {
+        public readonly AuthData? AuthData;
+        public readonly bool IsMobile;
+        public readonly Capabilities Capabilities;
+        public readonly string Environment;
         public readonly string ClosestRegion;
+        public TournamentInfo? TournamentInfo;
 
-		public ExternalAuthData(AuthData? authData, bool isMobile, Capabilities capabilities, string environment, string closestRegion)
-		{
-			AuthData = authData;
-			IsMobile = isMobile;
-			Capabilities = capabilities;
-			Environment = environment;
+        public ExternalAuthData(
+            AuthData? authData,
+            bool isMobile,
+            Capabilities capabilities,
+            string environment,
+            string closestRegion,
+            TournamentInfo? tournamentInfo)
+        {
+            TournamentInfo = tournamentInfo;
+            AuthData = authData;
+            IsMobile = isMobile;
+            Capabilities = capabilities;
+            Environment = environment;
             ClosestRegion = closestRegion;
         }
-	}
+    }
 }
--- Runtime/Scripts/Authorization/StandaloneExternalAuthorizerConfig.cs
+++ Runtime/Scripts/Authorization/StandaloneExternalAuthorizerConfig.cs
@@ -1,14 +1,35 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ElympicsLobbyPackage.Authorization
 {
-	[CreateAssetMenu(fileName = "StandaloneExternalAuthorizerConfig", menuName = "Configs/Authorizer/Standalone")]
-	public class StandaloneExternalAuthorizerConfig : ScriptableObject
-	{
-		public Capabilities Capabilities => capabilities;
+    [CreateAssetMenu(fileName = "StandaloneExternalAuthorizerConfig", menuName = "Configs/Authorizer/Standalone")]
+    public class StandaloneExternalAuthorizerConfig : ScriptableObject
+    {
+        public Capabilities Capabilities => capabilities;
         public string ClosestRegion => closestRegion;
+        public bool IsTournamentAvailable => isTournamentAvailable;
+        public string TournamentName => tournamentName;
+        public string OwnerId => ownerId;
+        public int TournamentState => tournamentState;
+        public string CreatedDate => createdDate;
+        public string StartDate => startDate;
+        public string EndDate => endDate;
+        public List<string> Participants => participants;
 
-		[SerializeField] private Capabilities capabilities;
+
+        [SerializeField] private Capabilities capabilities;
         [SerializeField] private string closestRegion;
+
+        [Header("Tournament")]
+        [SerializeField] private bool isTournamentAvailable;
+        [SerializeField] private string ownerId;
+        [SerializeField] private string tournamentName;
+        [SerializeField] private string createdDate;
+        [SerializeField] private string startDate;
+        [SerializeField] private string endDate;
+        [SerializeField] private int tournamentState;
+        [SerializeField] private List<string> participants;
     }
 }
